2f98f58a858178d8f869d766e7e2ce90d44cc245
mirthis@gmail.com <mirthis@gmail.com>
2023-11-13 20:19:42 +0000

    remove google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
-import { GoogleAnalytics } from 'nextjs-google-analytics';
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics trackPageViews />
       <Navbar />
       <Component {...pageProps} />
     </>
